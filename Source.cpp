6c6cd39fad327aac471c51f2e8894fb65349134e
Vaibhav Soni <vaibhav.soni@eronkan.com>
2015-11-23 23:22:29 +0530

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,12 @@
+/*
+ 
+ * Name         : Soucre.cpp
+ * Author		: Vaibhav Soni (vssonivaibhav6@gmail.com)
+ * Version		: 1.0
+ * License		: MIT Licensed
+ * Description		: 
+ *
+ */
 #include <iostream>
 #include <cstdio>
 #include <ctime>
@@ -6,22 +15,34 @@
 #include <fstream>
 #include <string>
 int main() {
-	std::clock_t start;
-	int count = 0;
-	double duration;
-	start = std::clock();
-	for (int i = 0; i < 25; i++)
+	
+	int filecount = 5;                          //No of file you want to generate 
+	int timegap = 6000;	                        //time gap you want to give between generation of files in millisecs 6000ms = 6s
+	std::clock_t start;							//Creating clock time variable which will store data in time format
+	double duration;							//Variable to store duration
+	start = std::clock();						//Starting the clock n saving present time to vairiable start
+	std::string fextention = ".txt";            //extension of file you want to create
+	std::string dirpath = "C:\\test\\";			//dirpath where you want files to be generated
+	
+		
+	for (int i = 0; i <filecount; i++)
 	{
-	std::this_thread::sleep_for(std::chrono::milliseconds(6000));
-	std::string txt = ".csv";
-	std::string filename = "C:\\test\\6a898dbf-9c09-4ff5-b0eb-fdbd928f2ba7_File"+ std::to_string(count) +txt;
-	std::ofstream OFileObject;  // Create Object of Ofstream
-	OFileObject.open(filename.c_str()); // Opening a File or creating if not present
-	OFileObject << "Temp:1"+ std::to_string(count)+",Pre:2"+std::to_string(i)+ ",Profit:3" + std::to_string(i) + ",Loss:4" + std::to_string(i); // Writing data to file 
-	std::cout << "Data has been written to file";
-	count++;
+		std::this_thread::sleep_for(std::chrono::milliseconds(timegap));	//thread which will make program sleep 
+		std::string filename = dirpath+"My_File"+ std::to_string(i) + fextention; 
+		std::ofstream OFileObject;											// Create Object of Ofstream
+		OFileObject.open(filename.c_str());									// Opening a File or creating if not present
+
+	// Now writing data into the genrated file
+		OFileObject << "Hey this data is for file " + std::to_string(i)+"\n";    
+		OFileObject << "This is the secound line of file \n";
+	//Console Message
+		std::cout << "Data has been written to file sucessfully";
+
 	OFileObject.close();
-	duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;
-    std::cout << "printf: " << duration << '\n';
+		duration = (std::clock() - start) / (double)CLOCKS_PER_SEC;        //daration it took to create and write in file
+
+    	std::cout << "printf: " << duration << '\n';
+	
 	}
+	return 0;
 }
